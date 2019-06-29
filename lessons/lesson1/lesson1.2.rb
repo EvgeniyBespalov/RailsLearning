85a3faa28500c0e7f991aab46b6fb53d8203ebd5
BespalovEvgeniy <bespalov1299@mail.ru>
2019-06-29 09:15:51 +0300

    lesson1.2

--- lessons/lesson1/lesson1.2.rb
+++ lessons/lesson1/lesson1.2.rb
@@ -1,2 +1,6 @@
-puts "What is triangls basis?"
-basis = gets.chomp.to_i
+puts "What is triangl's basis?"
+a = gets.chomp.to_i
+puts "What is triangl's height?"
+h = gets.chomp.to_i
+
+puts "Triangl's area is #{a * h / 2}"
