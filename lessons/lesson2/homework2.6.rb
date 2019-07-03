5bcdf39b66904e3f0333ff368823f9f1629c6082
BespalovEvgeniy <bespalov1299@mail.ru>
2019-07-03 10:32:17 +0300

    homework 2.6

--- lessons/lesson2/homework2.6.rb
+++ lessons/lesson2/homework2.6.rb
@@ -2,7 +2,7 @@ products = Hash.new
 
 loop do
 puts "Enter product name (enter stop to exit)"
-product = gets.chomp.lowcase
+product = gets.chomp.downcase
 
 break if product == "stop"
 
@@ -13,7 +13,7 @@ puts "Enter product quantity"
 quantity = gets.chomp.to_f
 
   if (products.key?(product))
-    if (products[product].key?[price])
+    if (products[product].key?(price))
       products[product][price] += quantity
     else
       products[product][price] = quantity
@@ -22,3 +22,13 @@ quantity = gets.chomp.to_f
     products[product] = {price => quantity}
   end
 end
+
+puts products
+
+product_costs = Hash.new
+
+products.each{|k, v| product_costs[k] = v.map{|p, q| p*q}.sum.round(2) }
+
+product_costs.each{|k, v| puts "#{k} - #{v}"}
+
+puts "All costs = #{product_costs.map{|k, v| v}.sum.round(2)}"
